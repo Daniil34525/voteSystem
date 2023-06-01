aafc147f2f526f5b96cfbb742c969eac6a89f0cb
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2023-06-01 22:04:18 +0300

    Artem-styles additing 50%

--- views/hiddens/index.php
+++ views/hiddens/index.php
@@ -13,13 +13,15 @@ use yii\widgets\Pjax;
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
 $this->registerCssFile("@web/css/hiddens.css");
+$this->registerCssfile("@web/css/hiddens_index.css");
+
 $this->title = 'Анонимные участники';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="hiddens-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <hr/>
+    <hr />
 
     <div class="creation_menue">
         <?= Html::a('Создать участника', ['create'], ['class' => 'btn btn-success']) ?>
@@ -40,6 +42,20 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'class' => ActionColumn::class,
                 'template' => '{delete}',
+                'buttons' => [
+                    'delete' => function ($url, $model, $key) {
+                        return Html::a(
+                            '<svg aria-hidden="true" style="height:24;width:24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0048 48h288a48 48 0 0048-48V128H32zm272-256a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zM432 32H312l-9-19a24 24 0 00-22-13H167a24 24 0 00-22 13l-9 19H16A16 16 0 000 48v32a16 16 0 0016 16h416a16 16 0 0016-16V48a16 16 0 00-16-16z"></path></svg>',
+                            ['delete', 'id' => $key], 
+                            [
+                                'data' => [
+                                    'confirm' => 'Вы действительно хотите удалить данного анонимного участника?',
+                                    'method' => 'post',
+                                ],
+                            ]
+                        );
+                    },
+                ]
             ],
         ],
     ]); ?>
@@ -54,14 +70,14 @@ $url = Url::to(['create', 'count' => ''], true);
 
 $js = <<< JS
 
-var count = 0
 function myfunc(value) {
   
-    var create_hiddens = document.querySelector("#main > div > div > p > a"); 
+    var create_hiddens = document.querySelector("#main > div > div > div.creation_menue > a"); 
     create_hiddens.href = "$url" + value;
 }
 
 JS;
-$position = $this::POS_BEGIN;
+$position = $this::POS_END;
+
 $this->registerJs($js, $position);
 ?>
